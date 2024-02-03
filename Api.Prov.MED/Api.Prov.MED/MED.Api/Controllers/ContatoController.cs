65de63779f2d19ee91680cd93f836b91f69f7ed6
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2024-02-03 15:34:58 -0300

    Estrutura_Inicial

---
+++ Api.Prov.MED/Api.Prov.MED/MED.Api/Controllers/ContatoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MED.Api.Controllers
+{
+    [ApiController]
+    [Route("{controllert}")]
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
